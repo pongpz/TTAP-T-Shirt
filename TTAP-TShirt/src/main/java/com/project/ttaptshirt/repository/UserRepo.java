a88e146b0dd8579fbe4902e94092004b03051beb
Newb1eCode <tdaoxuan113@gmail.com>
2024-07-15 23:08:28 +0700

    tach view giua nhan vien va khach hang r nhe, hom sau toi lam tim kiem va filter nhe

--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/repository/UserRepo.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/repository/UserRepo.java
@@ -5,10 +5,13 @@ import com.project.ttaptshirt.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
 //    User findByUsername(String userName);
     Optional<User> findByEmail(String email);
+
+    List<User> findByCv_Ten(String cv);
 }
