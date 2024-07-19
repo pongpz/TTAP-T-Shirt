ad7a4d818c3e96b670d31461c286e8cb337bd6a4
Newb1eCode <tdaoxuan113@gmail.com>
2024-07-19 20:14:39 +0700

    them chuc nang tim kiem cho nhan vien va khach hang

--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/repository/UserRepo.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/repository/UserRepo.java
@@ -14,4 +14,6 @@ public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     List<User> findByCv_Ten(String cv);
+
+    List<User> findByHoTenContainingIgnoreCase(String name);
 }
