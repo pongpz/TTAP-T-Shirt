8508d48316d052f446b8c3f067418bce7a080a4c
pongpz <phuongttone@gmail.com>
2024-07-24 22:35:02 +0700

    update giao diện thuộc tính sp

--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/controller/KieuDangController.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/controller/KieuDangController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,31 +21,31 @@ public class KieuDangController {
     @GetMapping("/view")
     public String viewKieuDang(Model model){
         model.addAttribute("listKieuDang",kieuDangRepository.findAll());
-        return "/admin/kieudang/kieu-dang";
+        return "admin/thuoctinhsanpham/kieu-dang";
     }
 
     @PostMapping("/add")
     public String addKieuDang(KieuDang kieuDang){
         kieuDangRepository.save(kieuDang);
-        return "redirect:/kieu-dang/view";
+        return "redirect:/admin/kieu-dang/view";
     }
 
-    @PostMapping("/update")
+    @PostMapping("/update/{id}")
     public String updateKieuDang(KieuDang kieuDang){
         kieuDangRepository.save(kieuDang);
-        return "redirect:/kieu-dang/view";
+        return "redirect:/admin/kieu-dang/view";
     }
 
     @GetMapping("/delete")
-    public String deleteKieuDang(@RequestParam("id") Long id){
+    public String deleteKieuDang(@PathVariable("id") Long id){
         kieuDangRepository.delete(kieuDangRepository.getReferenceById(id));
-        return "redirect:/kieu-dang/view";
+        return "redirect:/admin/kieu-dang/view";
     }
 
-    @GetMapping("/detail")
-    public String detailKieuDang(@RequestParam("id") Long id, Model model){
+    @GetMapping("/update/{id}")
+    public String detailKieuDang(@PathVariable("id") Long id, Model model){
         model.addAttribute("detailKieuDang",kieuDangRepository.getReferenceById(id));
         model.addAttribute("listKieuDang",kieuDangRepository.findAll());
-        return "/admin/kieudang/kieu-dang";
+        return "admin/thuoctinhsanpham/update-kieu-dang";
     }
 }
--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/controller/NSXController.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/controller/NSXController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,31 +19,31 @@ public class NSXController {
     @GetMapping("/admin/nsx/view")
     public String viewChatLieu(Model model){
         model.addAttribute("listNSX",nsxi.findAll());
-        return "admin/chatlieu/viewNSX";
+        return "admin/thuoctinhsanpham/nha-san-xuat";
     }
 
     @PostMapping("/admin/nsx/add")
     public String addChatLieu(NSX nsx){
         nsxi.save(nsx);
-        return "redirect:/nsx/view";
+        return "redirect:/admin/nsx/view";
     }
 
-    @PostMapping("/admin/nsx/update")
+    @PostMapping("/admin/nsx/update/{id}")
     public String updateChatLieu(NSX nsx){
         nsxi.save(nsx);
-        return "redirect:/nsx/view";
+        return "redirect:/admin/nsx/view";
     }
 
     @GetMapping("/admin/nsx/delete")
-    public String deleteChatLieu(@RequestParam("id") Integer id){
+    public String deleteChatLieu(@PathVariable("id") Long id){
         nsxi.delete(nsxi.getReferenceById(id));
-        return "redirect:/nsx/view";
+        return "redirect:/admin/nsx/view";
     }
 
-    @GetMapping("/admin/nsx/detail")
-    public String detailChatLieu(@RequestParam("id") Integer id, Model model){
+    @GetMapping("/admin/nsx/update/{id}")
+    public String detailChatLieu(@PathVariable("id") Long id, Model model){
         model.addAttribute("detailNSX",nsxi.getReferenceById(id));
         model.addAttribute("listNSX",nsxi.findAll());
-        return "admin/chatlieu/viewNSX";
+        return "admin/thuoctinhsanpham/update-nha-san-xuat";
     }
 }
