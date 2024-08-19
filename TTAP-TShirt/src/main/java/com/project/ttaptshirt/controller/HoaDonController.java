cd5b3fd8e367ed963b7b26a0e936bce31209e8e5
trunganhishere <duongtrunganh204@gmail.com>
2024-08-19 22:19:31 +0700

    123

--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/controller/HoaDonController.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/controller/HoaDonController.java
@@ -48,37 +48,7 @@ public class HoaDonController {
                           @RequestParam(required = false, value="loaiDon") Integer loaiDon,
                           @RequestParam(value="ngayThanhToan", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date ngayThanhToan,
                           Model model){
-        List<HoaDon> ls = new ArrayList<>();
-        if(keyword.trim().isEmpty() && trangThai == null && loaiDon == null && ngayThanhToan == null){
-            ls = hr.findAll();
-        }else if(!keyword.trim().isEmpty() && trangThai == null && loaiDon == null && ngayThanhToan == null){
-            ls = hr.searchKeyword(keyword);
-        }else if(keyword.trim().isEmpty() && trangThai != null && loaiDon == null && ngayThanhToan == null){
-            ls = hr.searchTrangThai(trangThai);
-        }else if(keyword.trim().isEmpty() && trangThai == null && loaiDon != null && ngayThanhToan == null){
-            ls = hr.searchLoaiDon(loaiDon);
-        }else if(keyword.trim().isEmpty() && trangThai == null && loaiDon == null && ngayThanhToan != null){
-            ls = hr.searchNgayThanhToan(ngayThanhToan);
-        }else if(!keyword.trim().isEmpty() && trangThai != null && loaiDon == null && ngayThanhToan == null){
-            ls = hr.searchKeywordVaTrangThai(keyword,trangThai);
-        }else if(!keyword.trim().isEmpty() && trangThai == null && loaiDon != null && ngayThanhToan == null){
-            ls = hr.searchKeywordVaLoaiDon(keyword, loaiDon);
-        }else if(!keyword.trim().isEmpty() && trangThai == null && loaiDon == null && ngayThanhToan != null){
-            ls = hr.searchKeywordVaNgayThanhToan(keyword, ngayThanhToan);
-        }else if(!keyword.trim().isEmpty() && trangThai != null && loaiDon != null && ngayThanhToan == null){
-            ls = hr.searchKeywordTrangThaiLoaiDon(keyword, trangThai,loaiDon);
-        }else if(!keyword.trim().isEmpty() && trangThai == null && loaiDon != null && ngayThanhToan != null){
-            ls = hr.searchKeywordLoaiHDNgayThanhToan(keyword, loaiDon, ngayThanhToan);
-        }else if(!keyword.trim().isEmpty() && trangThai != null && loaiDon == null && ngayThanhToan != null){
-            ls = hr.searchKeywordTrangThaiNgayThanhToan(keyword, loaiDon, ngayThanhToan);
-        }else if(keyword.trim().isEmpty() && trangThai != null && loaiDon != null && ngayThanhToan != null){
-            ls = hr.searchLoaiDonTrangThaiNgayThanhToan(loaiDon,trangThai,ngayThanhToan);
-        }else if(keyword.trim().isEmpty() && trangThai == null && loaiDon != null && ngayThanhToan != null){
-            ls = hr.searchLoaiDonNgayThanhToan(loaiDon,ngayThanhToan);
-        }else {
-            System.out.println("Có lỗi sảy ra");
-        }
-        model.addAttribute("listHD",ls);
+        model.addAttribute("listHD",hr.search(keyword, trangThai, loaiDon, ngayThanhToan));
         return"admin/hoadon/hoa-don";
     }
 
