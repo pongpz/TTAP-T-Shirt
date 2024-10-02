4746df1343c93b0dd7c516de8ffeb9887c9119da
Newb1eCode <tdaoxuan113@gmail.com>
2024-10-02 22:51:48 +0700

    sua lai nhan vien

--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/entity/KhachHang.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/entity/KhachHang.java
@@ -1,4 +1,5 @@
 package com.project.ttaptshirt.entity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,11 +21,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
