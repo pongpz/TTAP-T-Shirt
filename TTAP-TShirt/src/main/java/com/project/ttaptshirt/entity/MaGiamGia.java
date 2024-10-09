13bf1643610c4ebd7bf12ed18b5d9f6e4ae746f8
pongpz <129265131+pongpz@users.noreply.github.com>
2024-10-09 19:30:51 +0700

    fix hiển thị bán hàng, sp

--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/entity/MaGiamGia.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/entity/MaGiamGia.java
@@ -26,59 +26,57 @@ import java.sql.Date;
 public class MaGiamGia {
 
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
 
-        @NotBlank
-        @Column(name = "ten")
-        private String ten;
+    @NotBlank
+    @Column(name = "ten")
+    private String ten;
 
-        @NotNull
-        @Column(name = "ngay_bat_dau")
-        private Date ngayBatDau;
+    @NotNull
+    @Column(name = "ngay_bat_dau")
+    private Date ngayBatDau;
 
-        @NotNull
-        @Column(name = "ngay_ket_thuc")
-        private Date ngayKetThuc;
+    @NotNull
+    @Column(name = "ngay_ket_thuc")
+    private Date ngayKetThuc;
 
-        @NotBlank
-        @Column(name = "hinh_thuc")
-        private Integer hinhThuc;
+    @NotBlank
+    @Column(name = "hinh_thuc")
+    private Integer hinhThuc;
 
-        @NotNull
-        @Column(name = "gia_tri_giam")
-        private Float giaTriGiam;
+    @NotNull
+    @Column(name = "gia_tri_giam")
+    private Float giaTriGiam;
 
-        @NotNull
-        @Column(name = "gia_tri_toi_thieu")
-        private Float giaTriGiamToiThieu;
+    @NotNull
+    @Column(name = "gia_tri_toi_thieu")
+    private Float giaTriGiamToiThieu;
 
-        @NotNull
-        @Column(name = "gia_tri_toi_da")
-        private Float giaTriToiDa;
+    @NotNull
+    @Column(name = "gia_tri_toi_da")
+    private Float giaTriToiDa;
 
-        @NotBlank
-        @Column(name = "trang_thai")
-        private String trangThai;
+    @NotBlank
+    @Column(name = "trang_thai")
+    private String trangThai;
 
-        @NotBlank
-        @Column(name = "ngay_tao")
-        private Date ngayTao;
+    @NotBlank
+    @Column(name = "ngay_tao")
+    private Date ngayTao;
 
-        @NotBlank
-        @Column(name = "ngay_sua")
-        private Date ngaySua;
+    @NotBlank
+    @Column(name = "ngay_sua")
+    private Date ngaySua;
 
-        @NotBlank
-        @Column(name = "ma")
-        private String ma;
+    @NotBlank
+    @Column(name = "ma")
+    private String ma;
 
+    @NotNull
+    @Column(name = "so_luong")
+    private Integer soLuong;
 
-        @NotNull
-        @Column(name = "so_luong")
-        private Integer soLuong;
-
-    }
-
+}
