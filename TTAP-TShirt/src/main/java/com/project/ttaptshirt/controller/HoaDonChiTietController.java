d8fee5eb1bf5c65f6a61e3907950cca7f17c3bb4
trunganhishere <duongtrunganh204@gmail.com>
2024-10-17 20:33:31 +0700

    fix bug hoa don

--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/controller/HoaDonChiTietController.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/controller/HoaDonChiTietController.java
@@ -3,10 +3,11 @@ package com.project.ttaptshirt.controller;
 import com.project.ttaptshirt.repository.HoaDonChiTietRepository;
 import com.project.ttaptshirt.repository.HoaDonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -20,8 +21,9 @@ public class HoaDonChiTietController {
 
     @GetMapping("/admin/hoa-don-chi-tiet/hien-thi")
     public String hienThi(@RequestParam Long id, Model model, @RequestParam(defaultValue = "0") Integer page){
+        Pageable pageab = PageRequest.of(page, 5);
         model.addAttribute("listHDCT",hdctr.getHoaDonChiTietByHoaDonId(id));
-        model.addAttribute("listHD", hdr.findAll());
+        model.addAttribute("listHD", hdr.getAllHD(pageab));
         model.addAttribute("page",page);
         return "admin/hoadon/hoa-don";
     }
