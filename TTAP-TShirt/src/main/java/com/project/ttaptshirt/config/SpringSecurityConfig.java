5cea329a5789238031fc809b7f9ff97bfeba42b0
pongpz <phuongttone@gmail.com>
2024-11-17 15:50:27 +0700

    .

--- TTAP-TShirt/src/main/java/com/project/ttaptshirt/config/SpringSecurityConfig.java
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/config/SpringSecurityConfig.java
@@ -40,7 +40,6 @@ public class SpringSecurityConfig {
         return http.build();
     }
 
-<<<<<<< HEAD
 //    @Bean
 //    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http.csrf().disable().authorizeHttpRequests()
@@ -48,9 +47,7 @@ public class SpringSecurityConfig {
 //
 //        return http.build();
 //    }
-=======
 
->>>>>>> 309144c021ea3563d136f06913414073fb340ddf
 
     @Bean
     WebSecurityCustomizer webSecurityCustomizer() {
