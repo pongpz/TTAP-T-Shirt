5caf82abdb90aa8d165b237255aad67d1ff7a61d
trunganhishere <duongtrunganh204@gmail.com>
2024-11-19 20:53:51 +0700

    bo sung thanh toan vnpay

---
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/config/vieqrResponse.java
@@ -0,0 +1,12 @@
+package com.project.ttaptshirt.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class vieqrResponse {
+        private String access_token;
+        private String token_type;
+        private int expires_in;
+}
