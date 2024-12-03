0b6f3876bf983d5820d09ecc79db1845a092e0ae
Newb1eCode <129987850+Newb1eCode@users.noreply.github.com>
2024-12-03 14:34:40 +0700

    chinh sửa hiện gia tiền và số lượng

---
+++ TTAP-TShirt/src/main/java/com/project/ttaptshirt/dto/ChiTietSanPhamDTO.java
@@ -0,0 +1,16 @@
+package com.project.ttaptshirt.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ChiTietSanPhamDTO {
+    private Double giaBan;
+    private Integer soLuong;
+}
